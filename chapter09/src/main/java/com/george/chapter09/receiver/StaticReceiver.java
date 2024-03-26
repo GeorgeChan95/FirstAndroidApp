630236365ba90245694529f38b1de71c392da461
George <george_95@126.com>
2024-03-26 14:26:09 +0800

    静态广播，震动

--- chapter09/src/main/java/com/george/chapter09/receiver/StaticReceiver.java
+++ chapter09/src/main/java/com/george/chapter09/receiver/StaticReceiver.java
@@ -3,6 +3,7 @@ package com.george.chapter09.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.util.Log;
 
 public class StaticReceiver extends BroadcastReceiver {
@@ -13,6 +14,9 @@ public class StaticReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent != null && intent.getAction().equals(STATIC_ACTION)) {
             Log.d(TAG, "接收到静态广播");
+            // 从系统服务中获取震动管理器
+            Vibrator vb = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            vb.vibrate(500); // 命令震动器吱吱个若干秒，这里的500表示500毫秒
         }
     }
 }
