8de1b73e49e4958feafddfff53f5f959e3e763aa
George <george_95@126.com>
2024-04-25 17:20:45 +0800

    基于RxHttp实现文件的下载和进度监听

--- chapter13/src/main/java/com/george/chapter13/OkhttpDownloadActivity.java
+++ chapter13/src/main/java/com/george/chapter13/OkhttpDownloadActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,6 +23,7 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import rxhttp.RxHttp;
 
 public class OkhttpDownloadActivity extends AppCompatActivity {
 
@@ -35,12 +38,22 @@ public class OkhttpDownloadActivity extends AppCompatActivity {
         tv_result = findViewById(R.id.tv_result);
         tv_progress = findViewById(R.id.tv_progress);
         iv_result = findViewById(R.id.iv_result);
-        findViewById(R.id.btn_download_image).setOnClickListener(v -> downloadImage());
-        findViewById(R.id.btn_download_file).setOnClickListener(v -> downloadFile());
+        findViewById(R.id.btn_download_image).setOnClickListener(v -> {
+            // 基于OkHttp实现文件下载
+//            downloadImage();
+            // 使用RxHttp实现文件下载
+            rxHttpDownloadImage();
+        });
+        findViewById(R.id.btn_download_file).setOnClickListener(v -> {
+            // 基于OkHttp实现文件下载
+//            downloadFile();
+            // 使用RxHttp实现文件下载, 并监听进度
+            rxHttpDownloadFile();
+        });
     }
 
     /**
-     * 图片下载
+     * 基于OkHttp实现图片下载
      */
     private void downloadImage() {
         tv_progress.setVisibility(View.GONE);
@@ -77,13 +90,6 @@ public class OkhttpDownloadActivity extends AppCompatActivity {
      * 文件下载
      */
     private void downloadFile() {
-//        RxHttp.get("http://...")  //第一步，确定请求类型，可以选择postForm、postJson等方法
-//                .asString()           //第二步，确定返回类型，这里返回String类型
-//                .subscribe(s -> {     //第三步，订阅观察者，第二步返回Observable对象
-//                    //请求成功
-//                }, throwable -> {
-//                    //请求失败
-//                });
         tv_progress.setVisibility(View.VISIBLE);
         iv_result.setVisibility(View.GONE);
 
@@ -128,5 +134,69 @@ public class OkhttpDownloadActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    /**
+     * 使用RxHttp实现图片的下载
+     */
+    private void rxHttpDownloadImage() {
+        tv_progress.setVisibility(View.VISIBLE);
+        iv_result.setVisibility(View.VISIBLE);
+
+        String path = String.format("%s/%s.jpg", getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString(), "123");
+        RxHttp.get("down/image")
+                .toDownloadObservable(path)
+                .onMainProgress(prog -> {
+                    int progress = prog.getProgress();
+                    long currentSize = prog.getCurrentSize();
+                    long totalSize = prog.getTotalSize();
+                    String detail = String.format("当前下载进度：%d%%, 已下载字节：%d, 总大小：%d", progress, currentSize, totalSize);
+                    tv_progress.setText(detail);
+                })
+                .subscribe(next -> {
+                    File file = new File(path);
+                    long length = file.length();
+                    String desc = String.format("文件大小：%d", length);
+                    runOnUiThread(() -> {
+                        tv_result.setText("下载图片返回：" + desc);
+                        Uri uri = Uri.fromFile(file);
+                        iv_result.setImageURI(uri);
+                    });
+
+                }, throwable -> {
+                    // 回到主线程操纵界面
+                    runOnUiThread(() -> tv_result.setText("下载网络图片报错："+throwable.getMessage()));
+                });
+    }
+
+    /**
+     * 使用RxHttp实现文件下载, 并监听进度
+     */
+    private void rxHttpDownloadFile() {
+        tv_progress.setVisibility(View.VISIBLE);
+        iv_result.setVisibility(View.GONE);
+
+        String path = String.format("%s/%s.zip", getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString(), "8888");
+        RxHttp.get("down/file")
+                .toDownloadObservable(path)
+                .onMainProgress(prog -> {
+                    int progress = prog.getProgress();
+                    long currentSize = prog.getCurrentSize();
+                    long totalSize = prog.getTotalSize();
+                    String detail = String.format("当前下载进度：%d%%, 已下载字节：%d, 总大小：%d", progress, currentSize, totalSize);
+                    tv_progress.setText(detail);
+                })
+                .subscribe(next -> {
+                    File file = new File(path);
+                    long length = file.length();
+                    String desc = String.format("文件大小：%d M", length/1024/1024);
+                    runOnUiThread(() -> {
+                        tv_result.setText("下载文件成功，返回：" + desc);
+                    });
+
+                }, throwable -> {
+                    // 回到主线程操纵界面
+                    runOnUiThread(() -> tv_result.setText("下载文件报错："+throwable.getMessage()));
+                });
     }
 }
