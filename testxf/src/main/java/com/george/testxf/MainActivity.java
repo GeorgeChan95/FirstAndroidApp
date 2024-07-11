68bc8482094abc3f904a4dbc00e3284719505909
George <george_95@126.com>
2024-07-11 14:30:48 +0800

    语音命令

--- testxf/src/main/java/com/george/testxf/MainActivity.java
+++ testxf/src/main/java/com/george/testxf/MainActivity.java
@@ -8,15 +8,26 @@ import android.Manifest;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.george.testxf.util.FucUtil;
+import com.george.testxf.util.JsonParser;
+import com.george.testxf.util.XmlParser;
 import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.GrammarListener;
 import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.LexiconListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
 import com.iflytek.cloud.SpeechEvent;
+import com.iflytek.cloud.SpeechRecognizer;
 import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.SynthesizerListener;
@@ -26,24 +37,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "GeorgeTag";
     private static boolean mscInitialize = false; // 是否初始化过
 
-    private View btn_yyhc; // 语音合成
-    private View btn_init; // SDK初始化
     private Toast mToast;
 
     private SpeechSynthesizer mTts; // 语音合成对象
+    private Button btn_yyhc; // 语音合成
+    private Button btn_init; // SDK初始化
+    private Button btn_yyml; // 语音命令
+    private Button btn_gxcd; // 更新词典
+    private Button btn_gxcd2; // 更新词典
+
+    // 本地语法构建路径
+    private String grmPath;
+    // 语音识别对象
+    private SpeechRecognizer mAsr;
+    // 本地词典
+    private String mLocalLexicon = null;
+    // 本地语法文件
+    private String mLocalGrammar = null;
+    // 识别引擎类型
+    private String mEngineType = null;
+    // 语法类型
+    private final String GRAMMAR_TYPE_BNF = "bnf";
+    int ret = 0;// 函数调用返回值
+    // 返回结果格式，支持：xml,json
+    private String mResultType = "json";
+    private TextView tv_result;
+    private boolean isUpdate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 语音合成
+        tv_result = findViewById(R.id.tv_result);
+
+        // 引擎初始化
         btn_init = findViewById(R.id.btn_init);
         btn_init.setOnClickListener(this);
 
         // 语音合成
         btn_yyhc = findViewById(R.id.btn_yyhc);
         btn_yyhc.setOnClickListener(this);
+
+        // 语音命令
+        btn_yyml = findViewById(R.id.btn_yyml);
+        btn_yyml.setOnClickListener(this);
+
+        // 更新词典
+        btn_gxcd = findViewById(R.id.btn_gxcd);
+        btn_gxcd.setOnClickListener(this);
+
+        // 更新词典
+        btn_gxcd2 = findViewById(R.id.btn_gxcd2);
+        btn_gxcd2.setOnClickListener(this);
     }
 
     @Override
@@ -63,7 +109,138 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
+        if (v.getId() == R.id.btn_yyml) { // 语音命令
+            // 本地语法构建路径
+            grmPath = getExternalFilesDir("msc").getAbsolutePath() + "/test";
+            // 初始化识别对象
+            mAsr = SpeechRecognizer.createRecognizer(this, mInitListener);
+            if (mAsr == null) {
+                Log.e(TAG, "masr is null");
+            }
 
+            mLocalGrammar = FucUtil.readFile(this, "call.bnf", "utf-8");
+            mEngineType = SpeechConstant.TYPE_LOCAL;
+
+            if (!isUpdate) { // 如果没有构建过语法,则先构建语法
+                // 构建本地语法
+                setLocalGrammar();
+            }
+
+            // 设置语音命令参数
+            if (!setAsrParam()) {
+                showTip("请先构建语法。");
+                return;
+            }
+
+            // 开始语音指令监听
+            ret = mAsr.startListening(mRecognizerListener);
+            if (ret != ErrorCode.SUCCESS) {
+                showTip("识别失败,错误码: " + ret + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+            }
+        }
+
+        if (v.getId() == R.id.btn_gxcd) { // 更新词典
+            // 初始化语法、命令词
+            mLocalLexicon = "张海洋\n宋丹丹\n赵本山";
+            String mContent = new String(mLocalLexicon);
+
+            mAsr.setParameter(SpeechConstant.PARAMS, null);
+            // 设置引擎类型
+            mAsr.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
+            // 设置资源路径
+            mAsr.setParameter(ResourceUtil.ASR_RES_PATH, getAsrResourcePath());
+            //使用8k音频的时候请解开注释
+//				mAsr.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
+            // 设置语法构建路径
+            mAsr.setParameter(ResourceUtil.GRM_BUILD_PATH, grmPath);
+            // 设置语法名称
+            mAsr.setParameter(SpeechConstant.GRAMMAR_LIST, "call");
+            // 设置文本编码格式
+            mAsr.setParameter(SpeechConstant.TEXT_ENCODING, "utf-8");
+            ret = mAsr.updateLexicon("contact", mContent, lexiconListener);
+            if (ret != ErrorCode.SUCCESS) {
+                showTip("更新词典失败,错误码：" + ret + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+            }
+            isUpdate = true;
+        }
+
+        if (v.getId() == R.id.btn_gxcd2) { // 更新词典
+            mAsr.setParameter(SpeechConstant.PARAMS, null);
+            // 设置引擎类型
+            mAsr.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
+            // 设置资源路径
+            mAsr.setParameter(ResourceUtil.ASR_RES_PATH, getAsrResourcePath());
+            //使用8k音频的时候请解开注释
+//				mAsr.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
+            // 设置语法构建路径
+            mAsr.setParameter(ResourceUtil.GRM_BUILD_PATH, grmPath);
+            // 设置语法名称
+            mAsr.setParameter(SpeechConstant.GRAMMAR_LIST, "call");
+            // 设置文本编码格式
+            mAsr.setParameter(SpeechConstant.TEXT_ENCODING, "utf-8");
+
+            // 连续的更新词典,只有第一次会成功,
+            String move = "发短信\n打电话\n发微信";
+            ret = mAsr.updateLexicon("callPhone", move, lexiconListener);
+            if (ret != ErrorCode.SUCCESS) {
+                showTip("更新词典失败,错误码：" + ret + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+            }
+
+            isUpdate = true;
+        }
+
+    }
+
+    /**
+     * 设置语音命令参数
+     */
+    private boolean setAsrParam() {
+        boolean result = false;
+        // 清空参数
+        mAsr.setParameter(SpeechConstant.PARAMS, null);
+        // 设置识别引擎
+        mAsr.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+
+        // 设置本地识别资源
+        mAsr.setParameter(ResourceUtil.ASR_RES_PATH, getAsrResourcePath());
+        // 设置语法构建路径
+        mAsr.setParameter(ResourceUtil.GRM_BUILD_PATH, grmPath);
+        // 设置返回结果格式
+        mAsr.setParameter(SpeechConstant.RESULT_TYPE, mResultType);
+        // 设置本地识别使用语法id
+        mAsr.setParameter(SpeechConstant.LOCAL_GRAMMAR, "call");
+        // 设置识别的门限值
+        mAsr.setParameter(SpeechConstant.MIXED_THRESHOLD, "30");
+        // 使用8k音频的时候请解开注释
+//			mAsr.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
+        result = true;
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mAsr.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mAsr.setParameter(SpeechConstant.ASR_AUDIO_PATH,
+                getExternalFilesDir("msc").getAbsolutePath() + "/asr.wav");
+        return result;
+    }
+
+    /**
+     * 设置本地语法
+     */
+    private void setLocalGrammar() {
+        mAsr.setParameter(SpeechConstant.PARAMS, null);
+        // 设置文本编码格式
+        mAsr.setParameter(SpeechConstant.TEXT_ENCODING, "utf-8");
+        // 设置引擎类型
+        mAsr.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置语法构建路径
+        mAsr.setParameter(ResourceUtil.GRM_BUILD_PATH, grmPath);
+        //使用8k音频的时候请解开注释
+//					mAsr.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
+        // 设置资源路径
+        mAsr.setParameter(ResourceUtil.ASR_RES_PATH, getAsrResourcePath());
+        //
+        ret = mAsr.buildGrammar(GRAMMAR_TYPE_BNF, mLocalGrammar, grammarListener);
+        if (ret != ErrorCode.SUCCESS) {
+            showTip("语法构建失败,错误码：" + ret + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+        }
     }
 
     /**
@@ -133,7 +310,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //设置合成
         mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_XTTS);
         //设置发音人资源路径
-        mTts.setParameter(ResourceUtil.TTS_RES_PATH, getResourcePath());
+        mTts.setParameter(ResourceUtil.TTS_RES_PATH, getXttsResourcePath());
         //设置发音人
         mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");
 
@@ -161,7 +338,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 获取发音人资源路径
      * @return
      */
-    private String getResourcePath() {
+    private String getXttsResourcePath() {
         StringBuffer tempBuffer = new StringBuffer();
         String type = "xtts";
         //合成通用资源
@@ -169,6 +346,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tempBuffer.append(";");
         //发音人资源
         tempBuffer.append(ResourceUtil.generateResourcePath(this, ResourceUtil.RESOURCE_TYPE.assets, type + "/" + "xiaoyan.jet"));
+        return tempBuffer.toString();
+    }
+
+    //获取识别资源路径
+    private String getAsrResourcePath() {
+        StringBuffer tempBuffer = new StringBuffer();
+        //识别通用资源
+        tempBuffer.append(ResourceUtil.generateResourcePath(this, ResourceUtil.RESOURCE_TYPE.assets, "asr/common.jet"));
         return tempBuffer.toString();
     }
 
@@ -230,6 +415,111 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				byte[] buf = obj.getByteArray(SpeechEvent.KEY_EVENT_TTS_BUFFER);
 				Log.e("MscSpeechLog", "buf is =" + buf);
 			}*/
+        }
+    };
+
+
+    /**
+     * 初始化监听器。
+     */
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            Log.d(TAG, "SpeechRecognizer init() code = " + code);
+            if (code != ErrorCode.SUCCESS) {
+                showTip("初始化失败,错误码：" + code + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+            }
+        }
+    };
+
+    /**
+     * 构建语法监听器。
+     */
+    private GrammarListener grammarListener = new GrammarListener() {
+        @Override
+        public void onBuildFinish(String grammarId, SpeechError error) {
+            if (error == null) {
+                showTip("语法构建成功：" + grammarId);
+            } else {
+                showTip("语法构建失败,错误码：" + error.getErrorCode());
+            }
+        }
+    };
+
+    /**
+     * 识别监听器。
+     */
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+            showTip("当前正在说话，音量大小：" + volume);
+            Log.d(TAG, "返回音频数据：" + data.length);
+        }
+
+        @Override
+        public void onResult(final RecognizerResult result, boolean isLast) {
+            if (null != result && !TextUtils.isEmpty(result.getResultString())) {
+                Log.d(TAG, "recognizer result：" + result.getResultString());
+                String text = "";
+                if (mResultType.equals("json")) {
+                    text = JsonParser.parseGrammarResult(result.getResultString(), mEngineType);
+                } else if (mResultType.equals("xml")) {
+                    text = XmlParser.parseNluResult(result.getResultString());
+                } else {
+                    text = result.getResultString();
+                }
+
+                tv_result.setText(text);
+
+            } else {
+                Log.d(TAG, "recognizer result : null");
+            }
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+            showTip("结束说话");
+        }
+
+        @Override
+        public void onBeginOfSpeech() {
+            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+            showTip("开始说话");
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            showTip("onError Code：" + error.getErrorCode());
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            //		Log.d(TAG, "session id =" + sid);
+            //	}
+        }
+
+    };
+
+    /**
+     * 更新词典监听器。
+     */
+    private LexiconListener lexiconListener = new LexiconListener() {
+        @Override
+        public void onLexiconUpdated(String lexiconId, SpeechError error) {
+            if (error == null) {
+                Log.d(TAG, "词典更新成功");
+                showTip("词典更新成功");
+            } else {
+                Log.e(TAG, "词典更新失败, 错误码:" + error.getErrorCode());
+                showTip("词典更新失败,错误码：" + error.getErrorCode());
+            }
         }
     };
 
